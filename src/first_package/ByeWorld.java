110c64dd3f3cbfe697cb292f9e4c61ed8298b77e
Anam Zahra <zanam090@gmail.com>
2022-08-23 11:31:29 -0400

    added ByeWorld.java

---
+++ src/first_package/ByeWorld.java
@@ -0,0 +1,8 @@
+package first_package;
+
+public class ByeWorld {
+    public static void main(String[] args) {
+        System.out.println("May be 5 weeks of Java left");
+        System.out.println("May be 1.5 week of git");
+    }
+}
